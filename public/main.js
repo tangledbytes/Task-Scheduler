78a3d5412445d03c8f3afcca5b335c1c7b65f5af
Utkarsh <srivastavautkarsh8097@gmail.com>
2019-01-23 18:42:55 +0530

    Password confirmation added

---
+++ public/main.js
@@ -0,0 +1,15 @@
+document.getElementById("pass2").addEventListener('keyup', function(){
+    if(document.getElementById("pass1").value == document.getElementById("pass2").value){
+        document.querySelector('.message').classList.remove('fail');
+        document.querySelector('.alert').classList.add('success');
+        document.querySelector('.message').innerHTML = 'Password Match';
+        document.querySelector('.login-btn').classList.remove('disable');
+        document.querySelector('.login-btn').disabled = false;
+    }else{
+        document.querySelector('.message').classList.remove('success');
+        document.querySelector('.alert').classList.add('fail');
+        document.querySelector('.message').innerHTML = 'Password mismatch';
+        document.querySelector('.login-btn').classList.add('disable');
+        document.querySelector('.login-btn').disabled = true;
+    }
+})
