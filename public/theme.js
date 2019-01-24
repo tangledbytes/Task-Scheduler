dcec8c891f92fb699d0f57c8f6a86f7b3de153ca
Utkarsh <srivastavautkarsh8097@gmail.com>
2019-01-24 19:39:13 +0530

    Added hamburger menu

--- public/theme.js
+++ public/theme.js
@@ -1,3 +1,11 @@
+// w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+// var targetWidth = 470;
+// if ( w <= targetWidth) {     
+//     document.querySelector('.drop').style.display = 'none';
+// }else if(w > targetWidth){
+//     document.querySelector('.drop').style.display = 'flex';
+// }
+
 function swapStyleSheet(sheet) {
     document.querySelector('.stylesheet').setAttribute("href", sheet); 
     localStorage.theme = sheet; 
@@ -14,4 +22,23 @@ function initate() {
     style2.onclick = function () { swapStyleSheet("/stylesheets/home-dark.css"); };
 }
 
+// document.querySelector('.navs').addEventListener('click',function(){
+//     if(document.querySelector('.drop').style.display == 'none'){
+//         document.querySelector('.navs').style.backgroundColor = '#232626';
+//         document.querySelector('.drop').style.display = 'block';
+//     }
+//     else{
+//         document.querySelector('.navs').style.backgroundColor = 'white';
+//         document.querySelector('.drop').style.display = 'none';
+//     }
+// });
+document.querySelector('.hamburger').addEventListener('click',function(){
+    document.querySelector('.dropdown').classList.toggle('drop');
+    if(document.querySelector('.login-form').style.zIndex != -1){
+        document.querySelector('.login-form').style.zIndex = -1;
+    }
+    else{
+        document.querySelector('.login-form').style.zIndex = 0;
+    }
+});
 window.onload = initate;
